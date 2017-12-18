3f0850997a61785803d605817f00e2b0c2aee943
Luke Wang <lukewang.wyl@gmail.com>
2017-12-18 22:18:11 +0800

    Add CleanMyMac Chinese version

---
+++ Casks/cleanmymac-cn.rb
@@ -0,0 +1,48 @@
+cask 'cleanmymac-cn' do
+  version 'latest'
+  sha256 'a6898786e22488e382bcf64874b0e6ee5bc964a9ca88195728b844033c2771ff'
+
+  # devmate.com/com.macpaw.CleanMyMac was verified as official when first introduced to the cask
+  url 'https://dl.devmate.com/com.macpaw.zh.CleanMyMac3/CleanMyMacChinese.dmg'
+  name 'CleanMyMacChinese'
+  homepage 'https://macpaw.com/cleanmymac'
+
+  app "CleanMyMac 3.app"
+
+  uninstall delete:     [
+                          "/Library/PrivilegedHelperTools/com.macpaw.CleanMyMac#{version.major}.Agent",
+                          "/private/var/run/com.macpaw.CleanMyMac#{version.major}.Agent.socket",
+                        ],
+            launchctl:  [
+                          "com.macpaw.CleanMyMac#{version.major}.Agent",
+                          "com.macpaw.CleanMyMac#{version.major}.Scheduler",
+                        ],
+            login_item: "CleanMyMac #{version.major} Menu",
+            quit:       [
+                          "com.macpaw.CleanMyMac#{version.major}",
+                          "com.macpaw.CleanMyMac#{version.major}.Menu",
+                        ]
+
+  zap trash: [
+               "/Users/Shared/CleanMyMac #{version.major}",
+               "/Users/Shared/CleanMyMac #{version.major} Menu",
+               "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.macpaw.cleanmymac#{version.major}.sfl*",
+               "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.macpaw.cleanmymac#{version.major}.scheduler.sfl*",
+               "~/Library/Application Support/CleanMyMac #{version.major}",
+               "~/Library/Application Support/CleanMyMac #{version.major} Menu",
+               "~/Library/Caches/CleanMyMac #{version.major}",
+               "~/Library/Caches/com.apple.helpd/SDMHelpData/Other/English/HelpSDMIndexFile/com.macpaw.CleanMyMac#{version.major}.help*",
+               "~/Library/Caches/com.macpaw.CleanMyMac#{version.major}",
+               "~/Library/Caches/com.macpaw.CleanMyMac#{version.major}.KnowledgeBase",
+               "~/Library/Caches/com.macpaw.CleanMyMac#{version.major}.Menu",
+               "~/Library/Caches/com.macpaw.CleanMyMac#{version.major}.Scheduler",
+               "~/Library/Logs/CleanMyMac #{version.major}.log",
+               "~/Library/Logs/com.macpaw.CleanMyMac#{version.major}",
+               "~/Library/Preferences/com.macpaw.CleanMyMac-#{version.major}-Helper.plist",
+               "~/Library/Preferences/com.macpaw.CleanMyMac#{version.major}.KnowledgeBase.plist",
+               "~/Library/Preferences/com.macpaw.cleanmymac#{version.major}.menu.plist",
+               "~/Library/Preferences/com.macpaw.CleanMyMac#{version.major}.Scheduler.plist",
+               "~/Library/Preferences/com.macpaw.CleanMyMac#{version.major}.plist",
+               "~/Pictures/Photos Library.photoslibrary/private/com.macpaw.CleanMyMac#{version.major}",
+             ]
+end
