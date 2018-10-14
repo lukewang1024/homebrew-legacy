8b2a255c2e6ff4bddc0779fa6d4482753d5bbabe
Luke Wang <lukewang.wyl@gmail.com>
2018-10-14 21:48:56 +0800

    Add mweb2

---
+++ Casks/mweb2.rb
@@ -0,0 +1,12 @@
+cask 'mweb2' do
+  version '2.3.0'
+  sha256 'f1985e2c3e2e44e9243ac390663996c1ad2480166c52ca2bbcbe4a4623580fec'
+
+  # dl.devmate.com/com.coderforart.MWeb was verified as official when first introduced to the cask
+  url 'https://dl.devmate.com/com.coderforart.MWeb/MWeb-ProMarkdownwriting%2CnotetakingandstaticbloggeneratorApp.dmg'
+  appcast 'https://updates.devmate.com/com.coderforart.MWeb.xml'
+  name 'MWeb'
+  homepage 'http://www.mweb.im/'
+
+  app 'MWeb.app'
+end
